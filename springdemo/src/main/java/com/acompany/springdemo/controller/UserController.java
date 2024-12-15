e437d5dc2fc9f7e254590e25498289c36a35497a
Lucas0105 <dnjswo105@gmail.com>
2024-12-15 17:26:27 +0900

    feat: logger 실습

--- springdemo/src/main/java/com/acompany/springdemo/controller/UserController.java
+++ springdemo/src/main/java/com/acompany/springdemo/controller/UserController.java
@@ -3,6 +3,8 @@ package com.acompany.springdemo.controller;
 import com.acompany.springdemo.annotation.TokenRequired;
 import com.acompany.springdemo.model.User;
 import com.acompany.springdemo.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +13,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private static final Logger logger =
+            LoggerFactory.getLogger(UserController.class);
+
     private final UserService userService;
 
     @Autowired
@@ -26,6 +31,7 @@ public class UserController {
 
     @GetMapping("/{userId}")
     public User getUserById(@PathVariable Integer userId) {
+        logger.debug("" + userId);
         return userService.getUserByUserId(userId);
     }
 
